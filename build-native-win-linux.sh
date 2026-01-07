a4fee998e423809f67412d5f3db31dc3d7e368e9
phillip-haydon <phillip.haydon@gmail.com>
2026-01-08 09:18:43 +1300

    Fix Bundler version compatibility for TeamCity

--- build-native-win-linux.sh
+++ build-native-win-linux.sh
@@ -1,4 +1,6 @@
 export HOME=/usr/local/home
+# Update bundler version in lockfile to match installed bundler
+stdbuf -o0 bundle update --bundler || true
 stdbuf -o0 bundle config path vendor/cache
 stdbuf -o0 bundle install
 stdbuf -o0 bundle exec rake gem:native
